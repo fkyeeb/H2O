525eb967c69f21f5f81c67316c98cc83817fc803
fkyeeb <fky@umich.edu>
2025-10-08 15:30:32 -0400

    test ci errors

--- tests/test_4_map_dupl.py
+++ tests/test_4_map_dupl.py
@@ -48,7 +48,7 @@ def test_map_dupl():
     with open('tests/test_data/map_dupl_output/summary_tree_numbered.tre', 'r') as f:
         assert f.read() == '(o:0.0,((e:0.0,f:0.0)2:0.0,((a:0.0,b:0.0)4:0.0,(c:0.0,d:0.0)5:0.0)3:0.0)1:0.0)0:0.0;\n(o:0.0,((e:0.0,f:0.0)0:0.0,((a:0.0,b:0.0)0:0.0,(c:0.0,d:0.0)0:0.0)4:0.0)1:0.0)0:0.0;\n'
 
-    shutil.rmtree('tests/test_data/map_dupl_output')
+    # shutil.rmtree('tests/test_data/map_dupl_output')
 
 def test_map_dupl_no_output_directory():
     """Test the map_dupl function with no output directory"""
